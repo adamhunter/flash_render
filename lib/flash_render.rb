11757398a6b46c052a9da0bf3d6dbed6d53a20c5
Alex Ghiculescu <alexghiculescu@gmail.com>
2013-08-12 11:44:17 +1000

    use alias_method_chain so it plays nicely with other gems that override render

--- lib/flash_render.rb
+++ lib/flash_render.rb
@@ -1,6 +1,16 @@
 module FlashRender
-  
-  def render(*args)
+
+  def self.included(base)
+    # Protect from trying to augment modules that appear
+    # as the result of adding other gems.
+    return if base != ActionController::Base
+
+    base.class_eval do
+      alias_method_chain :render, :flash
+    end
+  end
+
+  def render_with_flash(*args)
     options = args.extract_options!
   
     if alert = options.delete(:alert)
@@ -18,9 +28,8 @@ module FlashRender
     end
   
     args << options
-    super(*args)
+    render_without_flash(*args)
   end
-
 end
 
 if defined?(Rails::Railtie)
