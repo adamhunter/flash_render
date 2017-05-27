2ce98dce4e2c2e907785e3a881b1364add003521
Dave Allie <dave@daveallie.com>
2017-05-28 01:38:40 +1000

    Avoid using alias_method_chain to make lib rails 5 compatible

--- lib/flash_render.rb
+++ lib/flash_render.rb
@@ -1,37 +1,26 @@
 module FlashRender
-
-  def self.included(base)
-    # Protect from trying to augment modules that appear
-    # as the result of adding other gems.
-    return unless base == ActionController::Base
-
-    base.alias_method_chain :render, :flash
-  end
-
-  def render_with_flash(*args)
+  def render(*args)
     options = args.extract_options!
-  
-    if alert = options.delete(:alert)
+
+    if (alert = options.delete(:alert))
       flash.now[:alert] = alert
     end
-  
-    if notice = options.delete(:notice)
+
+    if (notice = options.delete(:notice))
       flash.now[:notice] = notice
     end
-  
-    if other_flashes = options.delete(:flash)
+
+    if (other_flashes = options.delete(:flash))
       other_flashes.each do |key, value|
         flash.now[key] = value
       end
     end
-  
+
     args << options
-    render_without_flash(*args)
+    super(*args)
   end
 end
 
-if defined?(Rails::Railtie)
-  require 'flash_render/railtie'
-else
-  ActionController::Base.send :include, FlashRender
+unless defined?(Rails::Railtie)
+  ActionController::Base.prepend(FlashRender)
 end
